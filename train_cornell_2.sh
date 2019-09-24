dcd91050a3d385fd3578f947d49b7fc2be7fa859
Daniel Sawyer <sawyermade0@gmail.com>
2019-09-24 19:15:53 -0400

    lazy

--- train_cornell_2.sh
+++ train_cornell_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python3 train_ggcnn.py \
-	--description ggcnn2_50 \
+	--description ggcnn2_500 \
 	--network ggcnn2 \
 	--dataset cornell \
-	--dataset-path cornell_dataset/ \
-	--epochs 50 \
+	--dataset-path ~/DATASETS/cornell_dataset/ \
+	--epochs 500 \
 	--cuda $1
