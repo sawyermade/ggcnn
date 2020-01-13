37d37cceb61e7267c1d2fd74568c5558ac9b6758
smc <smc@pop-os.localdomain>
2020-01-13 11:40:36 -0500

    lazy

---
+++ realsense/eval_rs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+python3 eval_rs.py \
+	--network output/models/191206_1505_ggcnn2_50/epoch_44_iou_0.93 \
+	--dataset cornell \
+	--dataset-path /data/datasets/DATASETS/cornell_dataset \
+	--use-depth 1 \
+	--use-rgb 1 \
+	--split 0.9 \
+	--ds-rotate 0.0 \
+	--num-workers 8 \
+	--n-grasps 1 \
+	--cuda $1 \
+	--iou-eval #\
+	# --vis #\
+	# --augment #\
+	# --jacquard-output
